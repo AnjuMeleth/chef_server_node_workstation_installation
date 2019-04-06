cbeef62f498e9e9a53abcc10897911ca75eed064
AnjuMeleth <46922680+AnjuMeleth@users.noreply.github.com>
2019-04-06 11:17:42 +0530

    Update Chef_installation.sh

--- Chef_installation.sh
+++ Chef_installation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget https://packages.chef.io/files/stable/chefdk/3.7.23/ubuntu/18.04/chefdk_3.7.23-1_amd64.deb
-dpkg -i chefdk_3.7.23-1_amd64.deb
+sudo dpkg -i chefdk_3.7.23-1_amd64.deb
 which ruby
 echo 'eval "$(chef shell-init bash)"' >> ~/.bash_profile
 export PATH="/opt/chef/embedded/bin:$PATH"
